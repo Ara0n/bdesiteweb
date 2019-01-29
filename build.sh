a899a40a6dccebac6facba352aebf193d9a594ee
Thomas MONTERO <thomas99.montero@gmail.com>
2019-01-29 23:16:26 +0100

    fixed path in script

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 cp src/pages/* /var/www/bde-infossible.tk/html
-cp src/assets/* /var/www/bde-infossible.tk/assets
+cp src/assets/* /var/www/bde-infossible.tk/html/assets
 lessc src/styles/styles.less /var/www/bde-infossible.tk/html/res/styles.css
