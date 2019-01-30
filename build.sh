750f5a3b2dc9090d92b5a2ac93cba53fb57fdca2
Thomas MONTERO <thomas99.montero@gmail.com>
2019-01-30 16:53:32 +0100

    made it compatible with the new template build

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
-cp src/pages/* /var/www/bde-infossible.tk/html
+for i in "src/pages/"*; do
+	n="`echo $i | cut -d '/' -f 3`"
+	cat src/template/head.html | sed s/__ID_PAGE__/`echo $n | cut -d '.' -f 1` > /var/www/bde-infossible.tk/html/$n
+	cat $i src/template/foot.html >> /var/www/bde-infossible.tk/html/$n
+done
 cp src/assets/* /var/www/bde-infossible.tk/html/assets
 lessc src/styles/styles.less /var/www/bde-infossible.tk/html/res/styles.css
