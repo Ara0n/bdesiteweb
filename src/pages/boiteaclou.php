9c1f247e2662c3436b2d05a627d459d0326095c4
Thomas MONTERO <thomas99.montero@gmail.com>
2019-02-08 19:38:12 +0000

    php poll done (a big thanks to natnat-mc for it)

---
+++ src/pages/boiteaclou.php
@@ -0,0 +1,24 @@
+<?php
+$nom=$_POST['nom'];
+$idee=$_POST['idee'];
+
+if($nom!='' && $idee!='') {
+	$str=file_get_contents("boiteaclou.json");
+	$arr=json_decode($str);
+	$arr[]=[nom => $nom, idee => $idee];
+	$str=json_encode($arr);
+	file_put_contents("boiteaclou.json", $str);
+	?>
+	<span class="succes">Message envoyé avec succès!</span>
+	<?php
+}
+?>
+
+<h2>Boite questions/idées</h2>
+<form action="boiteaclou.php" method="POST">
+	<label for="nom">Nom</label>
+	<input type="text" name="nom" id="nom" />
+	<label for="idee">Idée</label>
+	<textarea name="idee" id="idee"></textarea>
+	<input type="submit" value="Envoyer" />
+</form>
